aba0ecb94cef5128adc4a63978302fda283fa05c
아쿠오수 <117049944+qwonv@users.noreply.github.com>
2022-10-31 11:19:45 +0900

    Create dscp1.js

---
+++ dscp1.js
@@ -0,0 +1,14 @@
+var Discord = require('discord.js');
+var client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'hello') {
+    msg.reply('lets fuck');
+  }
+});
+
+client.login('token');
